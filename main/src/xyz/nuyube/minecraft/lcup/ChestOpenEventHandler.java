d6ff50d6f31feac96c706d344f7a2b4ffa0cabe0
Nuyube <nuyubexz@gmail.com>
2021-10-10 11:36:54 -0400

    Add support for all containers

--- main/src/xyz/nuyube/minecraft/lcup/ChestOpenEventHandler.java
+++ main/src/xyz/nuyube/minecraft/lcup/ChestOpenEventHandler.java
@@ -1,39 +1,44 @@
 package xyz.nuyube.minecraft.lcup;
- 
+
 import org.bukkit.block.Block;
-import org.bukkit.block.Chest; 
-import org.bukkit.block.Sign; 
+import org.bukkit.block.Container;
+import org.bukkit.block.EnderChest;
+import org.bukkit.block.Sign;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.block.data.Directional;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.inventory.Inventory; 
+import org.bukkit.inventory.Inventory;
 
-  class ChestOpenEventHandler implements Listener {
+class ChestOpenEventHandler implements Listener {
     @EventHandler
-    //Hook the PlayerInteract event
-    public void onPlayerInteract(PlayerInteractEvent event){
-        //If we're right-clicking a block
-        if(event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+    // Hook the PlayerInteract event
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        // If we're right-clicking a block
+        if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
             Block clickedBlock = event.getClickedBlock();
-            
-            //And that block is a Sign
-            if(clickedBlock.getState() instanceof Sign) {
+
+            // And that block is a Sign
+            if (clickedBlock.getState() instanceof Sign) {
                 Player p = event.getPlayer();
-                //Get info about the sign
-                Sign S = (Sign)clickedBlock.getState();
-                //Get the block attached to the sign
-                Directional a = (Directional)S.getBlockData();
+                // Get info about the sign
+                Sign S = (Sign) clickedBlock.getState();
+                // Get the block attached to the sign
+                Directional a = (Directional) S.getBlockData();
                 Block Attached = clickedBlock.getRelative(a.getFacing().getOppositeFace());
-                //If that block is a Chest
-                if(Attached.getState() instanceof Chest) {
-                    //Open the inventory
-                    Chest c = (Chest)Attached.getState();
-                    Inventory i = c.getInventory(); 
-                    p.openInventory(i);  
-                } 
+                // If that block is an Ender Chest handle it differently
+                if (Attached.getState() instanceof EnderChest) {
+                    Inventory i = p.getEnderChest();
+                    p.openInventory(i);
+                }
+                // Otherwise handle it like a container
+                else if (Attached.getState() instanceof Container) {
+                    Container c = (Container) Attached.getState();
+                    Inventory i = c.getInventory();
+                    p.openInventory(i);
+                }
             }
         }
     }
